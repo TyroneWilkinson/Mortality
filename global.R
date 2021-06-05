2e6e1c755a2e167689a18cc54dc457fcaa5b5cba
unknown <tyronerwilkinson@gmail.com>
2021-06-05 18:38:05 -0400

    Uncommented code required to run.

--- global.R
+++ global.R
@@ -1,15 +1,16 @@
-# library(dplyr)
-# library(tidyr)
-# library(tigris)
-# options(tigris_use_cache = TRUE)
-# library(leaflet)
-# library(DT)
-# library(shiny)
-# library(shinydashboard)
-# library(sf)
-# library(ggplot2)
-# library(viridis)
-# ##############################################################################
+# install.packages(c("dplyr", "tidyr","tigris","leaflet","DT","shiny","shinydashboard","sf","ggplot2","viridis"))
+library(dplyr)
+library(tidyr)
+library(tigris)
+options(tigris_use_cache = TRUE)
+library(leaflet)
+library(DT)
+library(shiny)
+library(shinydashboard)
+library(sf)
+library(ggplot2)
+library(viridis)
+################################################################################
 # # STOP HERE AND GO TO LINE #209
 # ##############################################################################
 # ToTitle <- function(cvect) {
@@ -208,141 +209,141 @@
 # ###############################################################################
 # # START HERE; UNCOMMENT BELOW
 # ###############################################################################
-# # SAVE AND RESTORE DATA
-# #saveRDS(merged_df, file = './data/merged_df.RDS')
-# #saveRDS(states, file = './data/states.RDS')
-# #saveRDS(all.state.years, file = './data/allstateyears.RDS')
-# #saveRDS(country.years, file = './data/countryyears.RDS')
-# #saveRDS(og.country.years, file = './data/ogcountryyears.RDS')
-# 
-# merged_df <- readRDS(file = './data/merged_df.RDS')
-# states <- readRDS(file = './data/states.RDS')
-# all.state.years <- readRDS(file = './data/allstateyears.RDS')
-# country.years <- readRDS(file = './data/countryyears.RDS')
-# og.country.years <- readRDS(file = './data/ogcountryyears.RDS')
-# ###############################################################################
-# # Choices for sidebar menu (the causes of mortality)
-# choice <- colnames(merged_df)[3:27] %>% sort
-#################################################################################
-# ### R functions
-# # add in methods from https://github.com/rstudio/leaflet/pull/598
-# setCircleMarkerRadius <- function(map, layerId, radius, data=getMapData(map)){
-#   options <- list(layerId = layerId, radius = radius)
-#   # evaluate all options
-#   options <- evalFormula(options, data = data)
-#   # make them the same length (by building a data.frame)
-#   options <- do.call(data.frame, c(options, list(stringsAsFactors=FALSE)))
-#   leaflet::invokeMethod(map, data, "setRadius", options$layerId, options$radius)
-# }
-# 
-# setCircleMarkerStyle <- function(map, layerId
-#                                  , radius = NULL
-#                                  , stroke = NULL
-#                                  , color = NULL
-#                                  , weight = NULL
-#                                  , opacity = NULL
-#                                  , fill = NULL
-#                                  , fillColor = NULL
-#                                  , fillOpacity = NULL
-#                                  , dashArray = NULL
-#                                  , options = NULL
-#                                  , data = getMapData(map)
-# ){
-#   if (!is.null(radius)){
-#     setCircleMarkerRadius(map, layerId = layerId, radius = radius, data = data)
-#   }
-#   
-#   options <- c(list(layerId = layerId),
-#                options,
-#                filterNULL(list(stroke = stroke, color = color,
-#                                weight = weight, opacity = opacity,
-#                                fill = fill, fillColor = fillColor,
-#                                fillOpacity = fillOpacity, dashArray = dashArray
-#                )))
-#   
-#   if (length(options) < 2) { # no style options set
-#     return()
-#   }
-#   # evaluate all options
-#   options <- evalFormula(options, data = data)
-#   
-#   # make them the same length (by building a data.frame)
-#   options <- do.call(data.frame, c(options, list(stringsAsFactors=FALSE)))
-#   layerId <- options[[1]]
-#   style <- options[-1] # drop layer column
-#   
-#   #print(list(style=style))
-#   leaflet::invokeMethod(map, data, "setStyle", "marker", layerId, style);
-# }
-# 
-# setShapeStyle <- function( map, data = getMapData(map), layerId,
-#                            stroke = NULL, color = NULL,
-#                            weight = NULL, opacity = NULL,
-#                            fill = NULL, fillColor = NULL,
-#                            fillOpacity = NULL, dashArray = NULL,
-#                            smoothFactor = NULL, noClip = NULL,
-#                            options = NULL
-# ){
-#   options <- c(list(layerId = layerId),
-#                options,
-#                filterNULL(list(stroke = stroke, color = color,
-#                                weight = weight, opacity = opacity,
-#                                fill = fill, fillColor = fillColor,
-#                                fillOpacity = fillOpacity, dashArray = dashArray,
-#                                smoothFactor = smoothFactor, noClip = noClip
-#                )))
-#   # evaluate all options
-#   options <- evalFormula(options, data = data)
-#   # make them the same length (by building a data.frame)
-#   options <- do.call(data.frame, c(options, list(stringsAsFactors=FALSE)))
-#   
-#   layerId <- options[[1]]
-#   style <- options[-1] # drop layer column
-#   
-#   #print(list(style=style))
-#   leaflet::invokeMethod(map, data, "setStyle", "shape", layerId, style);
-# }
-# 
-# ### JS methods
-# leafletjs <-  tags$head(
-#   # add in methods from https://github.com/rstudio/leaflet/pull/598
-#   tags$script(HTML(
-#     '
-# window.LeafletWidget.methods.setStyle = function(category, layerId, style){
-#   var map = this;
-#   if (!layerId){
-#     return;
-#   } else if (!(typeof(layerId) === "object" && layerId.length)){ // in case a single layerid is given
-#     layerId = [layerId];
-#   }
-# 
-#   //convert columnstore to row store
-#   style = HTMLWidgets.dataframeToD3(style);
-#   //console.log(style);
-#   layerId.forEach(function(d,i){
-#     var layer = map.layerManager.getLayer(category, d);
-#     if (layer){ // or should this raise an error?
-#       layer.setStyle(style[i]);
-#     }
-#   });
-# };
-# 
-# window.LeafletWidget.methods.setRadius = function(layerId, radius){
-#   var map = this;
-#   if (!layerId){
-#     return;
-#   } else if (!(typeof(layerId) === "object" && layerId.length)){ // in case a single layerid is given
-#     layerId = [layerId];
-#     radius = [radius];
-#   }
-# 
-#   layerId.forEach(function(d,i){
-#     var layer = map.layerManager.getLayer("marker", d);
-#     if (layer){ // or should this raise an error?
-#       layer.setRadius(radius[i]);
-#     }
-#   });
-# };
-# '
-#   ))
-# )
+# SAVE AND RESTORE DATA
+#saveRDS(merged_df, file = './data/merged_df.RDS')
+#saveRDS(states, file = './data/states.RDS')
+#saveRDS(all.state.years, file = './data/allstateyears.RDS')
+#saveRDS(country.years, file = './data/countryyears.RDS')
+#saveRDS(og.country.years, file = './data/ogcountryyears.RDS')
+
+merged_df <- readRDS(file = './data/merged_df.RDS')
+states <- readRDS(file = './data/states.RDS')
+all.state.years <- readRDS(file = './data/allstateyears.RDS')
+country.years <- readRDS(file = './data/countryyears.RDS')
+og.country.years <- readRDS(file = './data/ogcountryyears.RDS')
+###############################################################################
+# Choices for sidebar menu (the causes of mortality)
+choice <- colnames(merged_df)[3:27] %>% sort
+################################################################################
+## R functions
+## add in methods from https://github.com/rstudio/leaflet/pull/598
+setCircleMarkerRadius <- function(map, layerId, radius, data=getMapData(map)){
+  options <- list(layerId = layerId, radius = radius)
+  # evaluate all options
+  options <- evalFormula(options, data = data)
+  # make them the same length (by building a data.frame)
+  options <- do.call(data.frame, c(options, list(stringsAsFactors=FALSE)))
+  leaflet::invokeMethod(map, data, "setRadius", options$layerId, options$radius)
+}
+
+setCircleMarkerStyle <- function(map, layerId
+                                 , radius = NULL
+                                 , stroke = NULL
+                                 , color = NULL
+                                 , weight = NULL
+                                 , opacity = NULL
+                                 , fill = NULL
+                                 , fillColor = NULL
+                                 , fillOpacity = NULL
+                                 , dashArray = NULL
+                                 , options = NULL
+                                 , data = getMapData(map)
+){
+  if (!is.null(radius)){
+    setCircleMarkerRadius(map, layerId = layerId, radius = radius, data = data)
+  }
+
+  options <- c(list(layerId = layerId),
+               options,
+               filterNULL(list(stroke = stroke, color = color,
+                               weight = weight, opacity = opacity,
+                               fill = fill, fillColor = fillColor,
+                               fillOpacity = fillOpacity, dashArray = dashArray
+               )))
+
+  if (length(options) < 2) { # no style options set
+    return()
+  }
+  # evaluate all options
+  options <- evalFormula(options, data = data)
+
+  # make them the same length (by building a data.frame)
+  options <- do.call(data.frame, c(options, list(stringsAsFactors=FALSE)))
+  layerId <- options[[1]]
+  style <- options[-1] # drop layer column
+
+  #print(list(style=style))
+  leaflet::invokeMethod(map, data, "setStyle", "marker", layerId, style);
+}
+
+setShapeStyle <- function( map, data = getMapData(map), layerId,
+                           stroke = NULL, color = NULL,
+                           weight = NULL, opacity = NULL,
+                           fill = NULL, fillColor = NULL,
+                           fillOpacity = NULL, dashArray = NULL,
+                           smoothFactor = NULL, noClip = NULL,
+                           options = NULL
+){
+  options <- c(list(layerId = layerId),
+               options,
+               filterNULL(list(stroke = stroke, color = color,
+                               weight = weight, opacity = opacity,
+                               fill = fill, fillColor = fillColor,
+                               fillOpacity = fillOpacity, dashArray = dashArray,
+                               smoothFactor = smoothFactor, noClip = noClip
+               )))
+  # evaluate all options
+  options <- evalFormula(options, data = data)
+  # make them the same length (by building a data.frame)
+  options <- do.call(data.frame, c(options, list(stringsAsFactors=FALSE)))
+
+  layerId <- options[[1]]
+  style <- options[-1] # drop layer column
+
+  #print(list(style=style))
+  leaflet::invokeMethod(map, data, "setStyle", "shape", layerId, style);
+}
+
+### JS methods
+leafletjs <-  tags$head(
+  # add in methods from https://github.com/rstudio/leaflet/pull/598
+  tags$script(HTML(
+    '
+window.LeafletWidget.methods.setStyle = function(category, layerId, style){
+  var map = this;
+  if (!layerId){
+    return;
+  } else if (!(typeof(layerId) === "object" && layerId.length)){ // in case a single layerid is given
+    layerId = [layerId];
+  }
+
+  //convert columnstore to row store
+  style = HTMLWidgets.dataframeToD3(style);
+  //console.log(style);
+  layerId.forEach(function(d,i){
+    var layer = map.layerManager.getLayer(category, d);
+    if (layer){ // or should this raise an error?
+      layer.setStyle(style[i]);
+    }
+  });
+};
+
+window.LeafletWidget.methods.setRadius = function(layerId, radius){
+  var map = this;
+  if (!layerId){
+    return;
+  } else if (!(typeof(layerId) === "object" && layerId.length)){ // in case a single layerid is given
+    layerId = [layerId];
+    radius = [radius];
+  }
+
+  layerId.forEach(function(d,i){
+    var layer = map.layerManager.getLayer("marker", d);
+    if (layer){ // or should this raise an error?
+      layer.setRadius(radius[i]);
+    }
+  });
+};
+'
+  ))
+)
